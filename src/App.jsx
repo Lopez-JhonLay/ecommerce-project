fbe663a098c5a7f87e3c93c183bbfe14abce69c1
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-08-12 00:39:14 +0800

    move checkout components into related folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { Routes, Route } from "react-router";
 
 import HomePage from "./pages/HomePage";
-import CheckoutPage from "./pages/CheckoutPage";
+import CheckoutPage from "./pages/checkout/CheckoutPage";
 import OrdersPage from "./pages/OrdersPage";
 import TrackingPage from "./pages/TrackingPage";
 
