62103e633d6d1a8f7596e70f2ad278acfb34ef9e
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-08-12 00:23:18 +0800

    move tracking page into react

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import CheckoutPage from "./pages/CheckoutPage";
 import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 
 function App() {
 	return (
@@ -20,6 +21,10 @@ function App() {
 			<Route
 				path="/orders"
 				element={<OrdersPage />}
+			/>
+			<Route
+				path="/tracking"
+				element={<TrackingPage />}
 			/>
 		</Routes>
 	);
