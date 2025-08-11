6fc5364abb45af176aeb78c26f99165cfb1a447f
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-08-12 00:08:02 +0800

    move checkout page into react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Routes, Route } from "react-router";
 
 import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
 	return (
